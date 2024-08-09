c8b42125414302972f35cdb7b980eb1217b1ff90
Megasploit <109837700+pq36@users.noreply.github.com>
2024-08-09 15:39:32 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import Songs from './songs';
+import Callback from './Callback';
+import Login from './login';
+import reportWebVitals from './reportWebVitals';
+import SearchResults from './searchResults';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/callback" element={<Callback />} />
+      <Route path="/songs" element={<Songs />} />
+      <Route path="/search/:searchTerm" element={<SearchResults />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  </BrowserRouter>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
